35d6a4f6c2b3813da8ee7b40a92b16274e452278
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-04-25 19:13:27 +0200

    modified: js/main.js

--- js/main.js
+++ js/main.js
@@ -23,36 +23,44 @@ const cursorFollow = $(".cursor-follow");
 const cursor = $(".cursor");
 const title = $(".title");
 const greetings = ["Hi", "Hello", "Hey", "Yo"];
+const isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(
+    navigator.userAgent
+  );
 
-$(document)
-  .on("mousemove", (e) => {
-    const x = e.pageX;
-    const y = e.pageY;
-    cursor.css({ left: x, top: y });
-    setTimeout(() => {
-      cursorFollow.css({ left: x, top: y });
-    }, 100);
-  })
-  .on("mouseenter", () => {
-    cursor.fadeIn(250);
-    cursorFollow.fadeIn(250);
-  })
-  .on("mouseleave", () => {
-    cursor.fadeOut(250);
-    cursorFollow.fadeOut(250);
-  });
+if (!isMobile) {
+  $(document)
+    .on("mousemove", (e) => {
+      const x = e.pageX;
+      const y = e.pageY;
+      cursor.css({ left: x, top: y });
+      setTimeout(() => {
+        cursorFollow.css({ left: x, top: y });
+      }, 100);
+    })
+    .on("mouseenter", () => {
+      cursor.fadeIn(250);
+      cursorFollow.fadeIn(250);
+    })
+    .on("mouseleave", () => {
+      cursor.fadeOut(250);
+      cursorFollow.fadeOut(250);
+    });
 
-$('[class*="action"]')
-  .on("mouseenter", () => {
-    cursorFollow.animate({ height: "40px", width: "40px" }, 250);
-  })
-  .on("mouseleave", () => {
-    cursorFollow.animate({ height: "25px", width: "25px" }, 250);
-  });
+  $('[class*="action"]')
+    .on("mouseenter", () => {
+      cursorFollow.animate({ height: "40px", width: "40px" }, 250);
+    })
+    .on("mouseleave", () => {
+      cursorFollow.animate({ height: "25px", width: "25px" }, 250);
+    });
+}
 
-title.fadeIn(800).on("dblclick", () => {
-  title.fadeOut(250, () => {
-    title.text(choose(greetings) + " :)").fadeIn(250);
+$(() => {
+  title.fadeIn(800).on("dblclick", () => {
+    title.fadeOut(250, () => {
+      title.text(choose(greetings) + " :)").fadeIn(250);
+    });
   });
 });
 
