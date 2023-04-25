ee86f3e276dd23bbbce892e96f063fbf8612c196
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-04-25 18:51:20 +0200

    Initial commit
    Changes to be committed:
    	new file:   .gitignore
    	new file:   css/style.css
    	new file:   images/icon-256x256.ico
    	new file:   index.html
    	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,66 @@
+"use strict";
+
+(function animateTitle(title) {
+  let i = 0;
+  const intervalId = setInterval(function () {
+    document.title = title.slice(0, i + 1);
+    i++;
+    if (i >= title.length) {
+      clearInterval(intervalId);
+      setTimeout(function () {
+        animateTitle(title);
+      }, 2000);
+    }
+  }, 300);
+})("mk_");
+
+function choose(choices) {
+  const index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+
+const cursorFollow = $(".cursor-follow");
+const cursor = $(".cursor");
+const title = $(".title");
+const greetings = ["Hi", "Hello", "Hey", "Yo"];
+
+$(document)
+  .on("mousemove", (e) => {
+    const x = e.pageX;
+    const y = e.pageY;
+    cursor.css({ left: x, top: y });
+    setTimeout(() => {
+      cursorFollow.css({ left: x, top: y });
+    }, 100);
+  })
+  .on("mouseenter", () => {
+    cursor.fadeIn(250);
+    cursorFollow.fadeIn(250);
+  })
+  .on("mouseleave", () => {
+    cursor.fadeOut(250);
+    cursorFollow.fadeOut(250);
+  });
+
+$('[class*="action"]')
+  .on("mouseenter", () => {
+    cursorFollow.animate({ height: "40px", width: "40px" }, 250);
+  })
+  .on("mouseleave", () => {
+    cursorFollow.animate({ height: "25px", width: "25px" }, 250);
+  });
+
+title.fadeIn(800).on("dblclick", () => {
+  title.fadeOut(250, () => {
+    title.text(choose(greetings) + " :)").fadeIn(250);
+  });
+});
+
+// $(() => {
+//   const colors = ["#885fff", "#5215fc", "#410ed3", "#280886", "#12043e"];
+//   $('section[id*="section"]')
+//     .toArray()
+//     .forEach((element) => {
+//       $(element).css({ backgroundColor: choose(colors) });
+//     });
+// });
