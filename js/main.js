38df9a5324d5bd5ebc972f0a7264aa6731b334f9
madkarmaa <madkarma99@gmail.com>
2023-04-27 15:56:39 +0200

    cards on section 2
    	modified:   css/style.css
    	modified:   index.html
    	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(
+    navigator.userAgent
+  );
+
 (function animateTitle(title) {
   let i = 0;
   const intervalId = setInterval(function () {
@@ -19,16 +24,10 @@ function choose(choices) {
   return choices[index];
 }
 
-const cursorFollow = $(".cursor-follow");
-const cursor = $(".cursor");
-const title = $(".title");
-const greetings = ["Hi", "Hello", "Hey", "Yo"];
-const isMobile =
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(
-    navigator.userAgent
-  );
-
 function customCursor() {
+  const cursorFollow = $(".cursor-follow");
+  const cursor = $(".cursor");
+
   $(document)
     .on("mousemove", (e) => {
       const x = e.pageX;
@@ -39,20 +38,20 @@ function customCursor() {
       }, 100);
     })
     .on("mouseenter", () => {
-      cursor.fadeIn(250);
-      cursorFollow.fadeIn(250);
+      cursor.finish().fadeIn(250);
+      cursorFollow.finish().fadeIn(250);
     })
     .on("mouseleave", () => {
-      cursor.fadeOut(250);
-      cursorFollow.fadeOut(250);
+      cursor.finish().fadeOut(250);
+      cursorFollow.finish().fadeOut(250);
     });
 
   $('[class*="action"]')
     .on("mouseenter", () => {
-      cursorFollow.animate({ height: "40px", width: "40px" }, 250);
+      cursorFollow.finish().animate({ height: "40px", width: "40px" }, 250);
     })
     .on("mouseleave", () => {
-      cursorFollow.animate({ height: "25px", width: "25px" }, 250);
+      cursorFollow.finish().animate({ height: "25px", width: "25px" }, 250);
     });
 }
 
@@ -61,9 +60,50 @@ if (!isMobile) {
 }
 
 $(() => {
+  const title = $(".title");
+  const greetings = ["Hi", "Hello", "Hey", "Yo"];
+
   title.fadeIn(800).on("dblclick", () => {
     title.fadeOut(250, () => {
       title.text(choose(greetings) + " :)").fadeIn(250);
     });
+  });
+});
+
+$(() => {
+  const colors = ["#885fff", "#5215fc", "#410ed3"];
+
+  $('[class*="card"]')
+    .toArray()
+    .forEach((card) => {
+      const color = choose(colors);
+
+      $(card)
+        .css({
+          border: `1px solid ${color}`,
+          boxShadow: `0 3px 15px 2px ${color}`,
+        })
+        .on("mouseover", () => {
+          $(card).finish().animate({ backgroundColor: color }, 250);
+        })
+        .on("mouseleave", () => {
+          $(card).finish().animate({ backgroundColor: "transparent" }, 250);
+        });
+    });
+});
+
+$(() => {
+  ScrollReveal({ reset: true });
+  const cards = $('[class*="card"]').toArray();
+
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    ScrollReveal().reveal(card, { duration: 1000, delay: 200 * i });
+  }
+});
+
+$(() => {
+  $('footer[class="action noselect"]').on("click", () => {
+    window.open("https://github.com/madkarmaa", "_blank");
   });
 });
