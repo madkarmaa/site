599df5600aeddee3adbb595382aceb164f00b958
madkarmaa <madkarma99@gmail.com>
2023-05-02 13:52:26 +0200

    text containers
    	modified:   css/style.css
    	modified:   index.html
    	modified:   js/mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -6,5 +6,6 @@ $(() => {
     $(".title").css("font-size", "7vmax");
     $('label[for="custom-cursor"]').hide();
     $('div[id="custom-cursor"]').hide();
+    $(".text-container").css("top", "27.5%");
   }
 });
