d2556eae87a15742ffe75040bfeeb9ccc8bec66b
madkarmaa <madkarma99@gmail.com>
2023-05-06 11:50:07 +0200

    forced portrait mode on mobile devices 	modified:   index.html 	modified:   js/mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -8,6 +8,14 @@ $(() => {
     $('div[id="custom-cursor"]').hide();
     $(".text-container > .container-content").css("font-size", "smaller");
     $(".text-container:has(.container-content)").css("top", "45%");
+
+    screen.orientation.lock("portrait");
+
+    $(window).on("orientationchange", () => {
+      if (screen.availHeight < screen.availWidth) {
+        alert("Please use portrait!");
+      }
+    });
   } else {
     // Also present in the CSS file, but has compatibility issues.
     $(".text-container:has(.container-content)").css("top", "40%");
