2ddd49157099dbd0984ac351f36d15f525e78c66
madkarmaa <madkarma99@gmail.com>
2023-05-08 12:07:31 +0200

    better easter egg
    	modified:   js/easter-eggs.js

--- js/easter-eggs.js
+++ js/easter-eggs.js
@@ -12,8 +12,53 @@ $(() => {
 });
 
 $(() => {
+  let tapCount = 0;
+  const iframeYT = $(
+    '<iframe style="width: 100%; height: 100%; z-index: 100; position: fixed; top: 0; left: 0;" src="https://www.youtube.com/embed/f8mL0_4GeV0?rel=0&modestbranding=1&autohide=1&mute=0&showinfo=0&controls=0&autoplay=1" title="YouTube video player" frameborder="0" allowfullscreen allow="autoplay; fullscreen"></iframe>'
+  );
+
+  function openFullscreen(el) {
+    var el = document.documentElement,
+      rfs = // for newer Webkit and Firefox
+        el.requestFullscreen ||
+        el.webkitRequestFullScreen ||
+        el.mozRequestFullScreen ||
+        el.msRequestFullscreen;
+
+    if (typeof rfs != "undefined" && rfs) {
+      rfs.call(el);
+    } else if (typeof window.ActiveXObject != "undefined") {
+      // for Internet Explorer
+      var wscript = new ActiveXObject("WScript.Shell");
+      if (wscript != null) {
+        wscript.SendKeys("{F11}");
+      }
+    }
+  }
+
+  function payload() {
+    $("body").append(iframeYT);
+    openFullscreen(iframeYT);
+
+    $(window).on("fullscreenchange", () => {
+      if (
+        !(
+          (screen.availHeight || screen.height - 30) <= window.innerHeight &&
+          document.fullscreenElement
+        )
+      ) {
+        iframeYT.remove();
+      }
+    });
+  }
+
   $(window).on("touchstart", () => {
-    console.log("tap");
+    tapCount++;
+
+    if (tapCount === 10) {
+      payload();
+      tapCount = 0;
+    }
   });
 
   $(window).on("keypress", function (e) {
@@ -22,42 +67,7 @@ $(() => {
       $(this).on("keypress", function (e) {
         input += e.key;
         if (input === "magic") {
-          function openFullscreen(el) {
-            var el = document.documentElement,
-              rfs = // for newer Webkit and Firefox
-                el.requestFullscreen ||
-                el.webkitRequestFullScreen ||
-                el.mozRequestFullScreen ||
-                el.msRequestFullscreen;
-
-            if (typeof rfs != "undefined" && rfs) {
-              rfs.call(el);
-            } else if (typeof window.ActiveXObject != "undefined") {
-              // for Internet Explorer
-              var wscript = new ActiveXObject("WScript.Shell");
-              if (wscript != null) {
-                wscript.SendKeys("{F11}");
-              }
-            }
-          }
-
-          const iframeYT = $(
-            '<iframe style="width: 100%; height: 100%; z-index: 100; position: fixed; top: 0; left: 0;" src="https://www.youtube.com/embed/f8mL0_4GeV0?rel=0&modestbranding=1&autohide=1&mute=0&showinfo=0&controls=0&autoplay=1" title="YouTube video player" frameborder="0" allowfullscreen allow="autoplay; fullscreen"></iframe>'
-          );
-
-          $("body").append(iframeYT);
-          openFullscreen(iframeYT);
-
-          $(window).on("fullscreenchange", () => {
-            if (
-              !(
-                (screen.availHeight || screen.height - 30) <=
-                  window.innerHeight && document.fullscreenElement
-              )
-            ) {
-              iframeYT.remove();
-            }
-          });
+          payload();
         }
       });
     }
