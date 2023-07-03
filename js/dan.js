a5ec14ae6b1152ced350b6b0341e13a48c38f617
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-07-03 21:04:18 +0200

    Added automatic project title sync

--- js/dan.js
+++ js/dan.js
@@ -17,6 +17,7 @@ async function getMarkdownText(url) {
 getMarkdownText('https://raw.githubusercontent.com/madkarmaa/automatic-chatgpt-dan/master/README.md').then((data) => {
   html = converter.makeHtml(data);
   $('.container').append($(html));
+  document.title = $('h1').toArray()[0].textContent; // in case the project's title changes
   $('.container > p > a:contains("Back to top")').remove();
   $('img[alt*="screenshot"]').toArray()[0].style = 'margin: 0 auto; display: block;';
   $('a')
