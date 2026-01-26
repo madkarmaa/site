5a2971a182722f313bdc3ae45a5ca6a8389654e3
madkarmaa <madkarma99@gmail.com>
2026-01-26 15:30:14 +0100

    feat: add icons to cards

--- src/lib/components/molecules/GitHubRepoCard.svelte
+++ src/lib/components/molecules/GitHubRepoCard.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import type { GitHubRepo } from '$lib/github/schemas';
+	import Star from '~icons/material-symbols/star-rounded';
+	import Fork from '~icons/material-symbols/call-split-rounded';
 
 	type Props = { repo: GitHubRepo };
 	let { repo }: Props = $props();
@@ -11,7 +13,19 @@
 	rel="noopener noreferrer external"
 	class="group block overflow-hidden rounded-md border-2 border-secondary-950 transition-colors duration-200 hover:border-accent-700"
 >
-	<div class="overflow-hidden">
+	<div class="relative overflow-hidden">
+		<div
+			class="absolute top-2 right-2 z-10 flex items-center justify-center gap-1 rounded-md bg-background-800 px-2 py-1 text-xs text-text-50 transition-colors duration-200 group-hover:bg-accent-700"
+		>
+			<Star class="h-4 w-4" />
+			{repo.stargazers_count}
+		</div>
+		<div
+			class="absolute top-2 left-2 z-10 flex items-center justify-center gap-1 rounded-md bg-background-800 px-2 py-1 text-xs text-text-50"
+		>
+			<Fork class="h-4 w-4" />
+			{repo.forks_count}
+		</div>
 		<img
 			alt="GitHub Repo Card"
 			src={repo.picture_url}
@@ -23,7 +37,7 @@
 		<h2
 			class="jetbrains-mono-600 text-xl text-text-700 transition-colors duration-200 group-hover:text-accent-700"
 		>
-			{repo.full_name}
+			{repo.name}
 		</h2>
 		<p class="text-sm text-text-950">{repo.description ?? '<no-description />'}</p>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,14 @@
 	let { data }: PageProps = $props();
 </script>
 
-<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-	{#if data.repos[0]}
-		{#each data.repos[0] as repo (repo.id)}
-			<GitHubRepoCard {repo} />
-		{/each}
-	{:else}
-		<p class="col-span-full text-center text-gray-500">{data.repos[1].error}</p>
-	{/if}
-</div>
+<main class="mx-auto flex max-w-[90%] flex-col px-0 py-8 md:max-w-[80%] md:px-5">
+	<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+		{#if data.repos[0]}
+			{#each data.repos[0] as repo (repo.id)}
+				<GitHubRepoCard {repo} />
+			{/each}
+		{:else}
+			<p class="col-span-full text-center text-accent-500">{data.repos[1].error}</p>
+		{/if}
+	</div>
+</main>
