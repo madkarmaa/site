449a538b06c2fdac541a7165fa48b9615aeb4aae
madkarmaa <madkarma99@gmail.com>
2026-01-27 15:37:36 +0100

    fix: fix more grammar mistakes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -109,7 +109,7 @@
 			Outside of coding, I'm an obsessed {@render descLink(
 				'http://gym-rat.urbanup.com/4855932',
 				'gym rat'
-			)} &#40;yes, <span class="italic">really</span>&#41;. I love photography and videography;
+			)} &#40;yes, <span class="italic">really</span>&#41;. I also love photography and videography;
 			there's something special about capturing moments in time. See you at the gym, I guess
 			<span class="text-accent-800">;&#41;</span>
 		{/snippet}
