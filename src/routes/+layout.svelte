4842817c28b0c00654a98644abe37423a68dc3c9
madkarmaa <madkarma99@gmail.com>
2026-01-27 16:23:20 +0100

    feat: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import './layout.css';
 	import type { Snippet } from 'svelte';
 	import { page } from '$app/state';
-	import favicon from '$assets/favicon.svg';
+	import favicon from '$assets/favicon.ico';
 
 	type Props = { children: Snippet };
 	let { children }: Props = $props();
@@ -10,7 +10,7 @@
 
 <svelte:head>
 	<title>{page.url.pathname}</title>
-	<link rel="icon" href={favicon} />
+	<link rel="shortcut icon" href={favicon} type="image/x-icon" />
 </svelte:head>
 
 {@render children()}
