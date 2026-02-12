151b2a29e9341eded881ec49fcd087a039f6628f
madkarmaa <madkarma99@gmail.com>
2026-02-12 21:31:12 +0100

    refactor: remove console error

--- src/lib/lastfm/index.ts
+++ src/lib/lastfm/index.ts
@@ -45,14 +45,12 @@ export const userGetRecentTracks = async (username: string) => {
 		const data = await response.json();
 
 		const recentTracks = LastFmRecentTracksSchema.safeParse(data);
-		if (!recentTracks.success) {
-			console.error(recentTracks.error);
+		if (!recentTracks.success)
 			return err({
 				code: 'LASTFM_RECENT_TRACKS_PARSE_ERROR' as const,
 				message: 'Failed to parse recent tracks',
 				details: recentTracks.error.issues
 			});
-		}
 
 		return ok(recentTracks.data.recent_tracks);
 	} catch (error) {
