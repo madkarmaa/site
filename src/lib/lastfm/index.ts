96639868f3f0d350189bb0be6ae437a5c9f2312b
madkarmaa <madkarma99@gmail.com>
2026-02-12 21:04:26 +0100

    feat: add lastfm lib code

---
+++ src/lib/lastfm/index.ts
@@ -0,0 +1,53 @@
+import { ok, err } from '@madkarma/ts-utils/result';
+import { PUBLIC_LASTFM_API_KEY } from '$env/static/public';
+import { LastFmRecentTracksSchema, LastFmErrorResponseSchema } from './schemas';
+
+const API_BASE_URL = 'https://ws.audioscrobbler.com/2.0' as const;
+
+const FORBIDDEN_PARAMS = ['method', 'api_key'] as const;
+type Forbidden = (typeof FORBIDDEN_PARAMS)[number];
+
+const methodUrl = <Params extends Record<string, string> & { [K in Forbidden]?: never }>(
+	method: string,
+	params: Params
+) => {
+	const sanitizedParams = Object.fromEntries(
+		Object.entries(params)
+			.map(([key, value]) => [key, value.trim()])
+			.filter(([key, value]) => value && !FORBIDDEN_PARAMS.includes(key as Forbidden))
+	);
+
+	const query = new URLSearchParams({
+		method: method.trim().toLowerCase(),
+		api_key: PUBLIC_LASTFM_API_KEY,
+		...sanitizedParams
+	});
+
+	return `${API_BASE_URL}/?${query.toString()}&format=json` as const;
+};
+
+export const userGetRecentTracksUrl = (username: string) =>
+	methodUrl('user.getrecenttracks', { user: username.trim() });
+
+export const userGetRecentTracks = async (username: string) => {
+	try {
+		const response = await fetch(userGetRecentTracksUrl(username));
+		if (!response.ok) {
+			const data = await response.json();
+			const errorResponse = LastFmErrorResponseSchema.parse(data);
+			return err({
+				code: 'LASTFM_RECENT_TRACKS_FETCH_ERROR' as const,
+				message: errorResponse.message,
+				status: response.status
+			});
+		}
+
+		const data = await response.json();
+		const recentTracks = LastFmRecentTracksSchema.parse(data);
+		return ok(recentTracks);
+	} catch (error) {
+		const message =
+			error instanceof Error ? error.message : 'Unknown error while fetching recent tracks';
+		return err({ code: 'LASTFM_UNKNOWN_ERROR', message });
+	}
+};
---
+++ src/lib/lastfm/schemas.ts
@@ -0,0 +1,37 @@
+import * as z from 'zod';
+
+const LastFmImageSchema = z.object({
+	'#text': z.url().trim(),
+	size: z.enum(['small', 'medium', 'large', 'extralarge'])
+});
+const LastFmArtistSchema = z.object({ '#text': z.string().trim() });
+const LastFmAlbumSchema = z.object({ '#text': z.url().trim() });
+
+const LastFmAttrSchema = z.object({
+	nowplaying: z
+		.string()
+		.trim()
+		.transform((v) => v === 'true')
+		.optional()
+});
+
+const LastFmTrackSchema = z.object({
+	artist: LastFmArtistSchema,
+	image: z.array(LastFmImageSchema),
+	album: LastFmAlbumSchema,
+	name: z.string().trim(),
+	'@attr': LastFmAttrSchema.optional()
+});
+
+export const LastFmRecentTracksSchema = z.object({
+	recenttracks: z.object({
+		track: z.union([LastFmTrackSchema, z.array(LastFmTrackSchema)])
+	})
+});
+export type LastFmRecentTracks = z.infer<typeof LastFmRecentTracksSchema>;
+
+export const LastFmErrorResponseSchema = z.object({
+	error: z.number(),
+	message: z.string().trim()
+});
+export type LastFmErrorResponse = z.infer<typeof LastFmErrorResponseSchema>;
