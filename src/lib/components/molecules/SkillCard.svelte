1b0b978f1a8d589d5267cff4f1bc1d1e2d710bb2
madkarmaa <madkarma99@gmail.com>
2026-07-18 17:32:07 +0200

    style: match skill icons to accent

--- src/lib/components/molecules/SkillCard.svelte
+++ src/lib/components/molecules/SkillCard.svelte
@@ -18,7 +18,7 @@
 		class=" flex flex-col items-center justify-center gap-4 rounded-md bg-background-100 p-6 transition-colors"
 		transition:fly={{ y: 20, duration: 200 }}
 	>
-		<Icon class="size-12 text-secondary-900 transition-transform md:size-18 " />
+		<Icon class="size-12 text-accent-800 transition-transform md:size-18 " />
 		<p class="text-center outfit-600 text-base text-text-700 md:text-lg">{label}</p>
 	</div>
 {/if}
